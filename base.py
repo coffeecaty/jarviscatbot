61f617cf2cb569e4106343a3550eea3272c21339
coffeecaty <coffeecatyang@gmail.com>
2017-10-15 19:59:17 +0800

    重构代码2.062_聊天逻辑再修正

--- base.py
+++ base.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 from logprint import log
 from datetime import timedelta
-import user_date,helpdoc,user_command
+import user_date
+import helpdoc
+import user_command
 
 
 def start(bot, update):
@@ -102,14 +104,6 @@ def repeat(bot, update, args):
 
 def talk(bot, update):
     log(update)
-    if user_date.me.user_list.inornot(update.message.chat_id):
-        bot.sendMessage(
-            user_date.love.user_list.list[0],
-            text=update.message.text)
-    elif user_date.love.user_list.inornot(update.message.chat_id):
-        bot.sendMessage(
-            user_date.me.user_list.list[0],
-            text=update.message.text)
     if '喜欢' in update.message.text or 'love' in update.message.text or '爱' in update.message.text:
         if user_date.love.user_list.inornot(update.message.chat_id):
             bot.sendMessage(update.message.chat_id, text='小猫也爱大熊！最爱大熊了！！',
@@ -118,8 +112,10 @@ def talk(bot, update):
             bot.sendMessage(update.message.chat_id, text='小猫知道大猫最爱大熊了！！',
                             reply_to_message_id=update.message.message_id)
         else:
-            bot.sendMessage(update.message.chat_id, text='你喜欢谁和小猫我没什么关系，小猫只爱大熊！',
-                            reply_to_message_id=update.message.message_id)
+            bot.sendMessage(
+                update.message.chat_id,
+                text='你喜欢谁和小猫我没什么关系，小猫只爱大熊！',
+                reply_to_message_id=update.message.message_id)
     elif '使用帮助' in update.message.text:
         bot.sendMessage(update.message.chat_id, text='小猫正在替你寻找使用帮助，不要着急哦',
                         reply_to_message_id=update.message.message_id)
@@ -129,24 +125,33 @@ def talk(bot, update):
                         reply_to_message_id=update.message.message_id)
         for n in user_date.for_group:
             if n.name in update.message.text:
-                user_command.apply(bot,update,[n.name])
+                user_command.apply(bot, update, [n.name])
     elif '屏蔽消息' in update.message.text:
         bot.sendMessage(update.message.chat_id, text='小猫正在替你处理你的要求，不要着急哦',
                         reply_to_message_id=update.message.message_id)
         for n in user_date.for_group:
             if n.name in update.message.text:
-                user_command.mute(bot,update,[n.name])
+                user_command.mute(bot, update, [n.name])
     elif '解除屏蔽' in update.message.text:
         bot.sendMessage(update.message.chat_id, text='小猫正在替你处理你的要求，不要着急哦',
                         reply_to_message_id=update.message.message_id)
         for n in user_date.for_group:
             if n.name in update.message.text:
-                user_command.unmute(bot,update,[n.name])
+                user_command.unmute(bot, update, [n.name])
     elif '调戏' in update.message.text:
         bot.sendMessage(update.message.chat_id, text='调戏小猫小心被小猫咬死哦V_V',
                         reply_to_message_id=update.message.message_id)
     else:
-        bot.sendMessage(
-            update.message.chat_id,
-            text='小猫听不懂你在说什么，小猫只会爱大熊！如果你想给coffeecaty或者summyxy留言，请使用/stc（say to coffeecaty）或者/sts（say to summyxy）',
-            reply_to_message_id=update.message.message_id)
+        if user_date.me.user_list.inornot(update.message.chat_id):
+            bot.sendMessage(
+                user_date.love.user_list.list[0],
+                text=update.message.text)
+        elif user_date.love.user_list.inornot(update.message.chat_id):
+            bot.sendMessage(
+                user_date.me.user_list.list[0],
+                text=update.message.text)
+        else:
+            bot.sendMessage(
+                update.message.chat_id,
+                text='小猫听不懂你在说什么，小猫只会爱大熊！如果你想给coffeecaty或者summyxy留言，请使用/stc（say to coffeecaty）或者/sts（say to summyxy）',
+                reply_to_message_id=update.message.message_id)
